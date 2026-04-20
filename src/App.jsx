9a64cb3aafc27fe6978169d4a2762fc2529dee83
Kaajal Shah <kaajals@uci.edu>
2026-04-19 20:52:47 -0700

    Initial commit - added file structure

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-black min-h-screen text-white">
+      <h1 className="text-4xl font-bold p-8">My Portfolio</h1>
+    </div>
+  )
+}
+
+export default App
